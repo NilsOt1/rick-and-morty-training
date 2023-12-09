628f4fa2bace04f2345a064b56cf5bf7aec8e3f6
nils <nilsotto@gmx.de>
2023-12-09 16:33:56 +0100

    add Navigation and Welcome component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-
 import './App.css'
 import Header from "./Header.tsx";
+import {Route, Routes} from "react-router-dom";
+import Welcome from "./Welcome.tsx";
+import Navigation from "./Navigation.tsx";
 
 function App() {
 
-  return (
-    <>
-      <Header/>
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <Navigation/>
+            <Routes>
+                <Route path={"/"} element={<Welcome/>}/>
+
+            </Routes>
+        </>
+    )
 }
 
 export default App
---
+++ src/Welcome.tsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+    return (
+        <>
+
+        <h2>Welcome! On this page, you can find, search and create Rick and Morty Characters</h2>
+        </>
+    )
+}
